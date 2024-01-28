21c30820a10ecf52e265d06e461608b68d00767c
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2024-01-29 00:31:12 +0200

    Summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import { useGetThemoviByTimeQuery } from './services/themoviedb'
-import './App.css'
+import { useGetThemoviByTimeQuery } from './services/themoviedb';
+import './App.css';
 
 function App() {
-  const [typeMedia , setTypeMedia] = useState('all');
+  const [typeMedia, setTypeMedia] = useState('all');
   const [trendingTime, setTrendingTime] = useState('day');
-  const { data, error, isLoading } = useGetThemoviByTimeQuery({ time: trendingTime, type: typeMedia })
-  
-  console.log(data)
+  const { data, error, isLoading } = useGetThemoviByTimeQuery({
+    time: trendingTime,
+    type: typeMedia,
+  });
 
-  const handleTimeChange = (event) => {
+  const handleTimeChange = event => {
     setTrendingTime(event.target.value);
   };
 
-    const handleTypeChange = (event) => {
+  const handleTypeChange = event => {
     setTypeMedia(event.target.value);
   };
 
@@ -30,33 +31,43 @@ function App() {
         </select>
       </label>
       <hr />
-       <label className="Label">
-      Trending time: 
-      <select name="trendingTime" onChange={handleTimeChange} defaultValue="day">  
+      <label className="Label">
+        Trending time:
+        <select
+          name="trendingTime"
+          onChange={handleTimeChange}
+          defaultValue="day"
+        >
           <option value="day">Day</option>
-          <option value="week">Week</option>  
+          <option value="week">Week</option>
         </select>
-        </label>
+      </label>
       {error ? (
         <>Oh no, there was an error</>
       ) : isLoading ? (
         <>Loading...</>
       ) : data ? (
         <>
-           <ul>
-      {data.
-results.map(type => (
-        <li key={type.id}>
-          <div>
-            {type.title || type.name}
-          </div>
-        </li>
-      ))}
-    </ul>
+          <ul>
+            {data.results.map(type => (
+              <li key={type.id} className="Items">
+                <span
+                  className="Item"
+                  style={{
+                    '--poster-path': `url('https://image.tmdb.org/t/p/w500/${
+                      type.poster_path || type.profile_path
+                    }')`,
+                  }}
+                >
+                  {type.title || type.name}
+                </span>
+              </li>
+            ))}
+          </ul>
         </>
       ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/themoviedb.js
+++ src/services/themoviedb.js
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BEARER = import.meta.env.VITE_BEARER;
+const BASE_URL = import.meta.env.VITE_URL
 
 export const themoviApi = createApi({
   reducerPath: 'themoviApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.themoviedb.org/3/',
+    baseUrl: `${BASE_URL}`,
     prepareHeaders: (headers) => {      
       headers.set('Authorization', `Bearer ${BEARER}`);
       return headers;
